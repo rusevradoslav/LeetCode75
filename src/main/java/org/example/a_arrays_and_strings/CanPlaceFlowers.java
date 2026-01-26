8748241a2260d930aed105422350a7c391d4cb72
radoslarusev <radorusevcrypto@gmail.com>
2026-01-26 19:31:24 +0200

    Small code optimization for CanPlaceFlowers problem

--- src/main/java/org/example/a_arrays_and_strings/CanPlaceFlowers.java
+++ src/main/java/org/example/a_arrays_and_strings/CanPlaceFlowers.java
@@ -35,7 +35,7 @@ public class CanPlaceFlowers {
         int size = flowerbed.length;
         for (int i = 0; i < size; i++) {
             boolean left = (i == 0) || (flowerbed[i - 1] == 0);
-            boolean right = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0);
+            boolean right = (i == size - 1 || flowerbed[i + 1] == 0);
 
             if (left && right && flowerbed[i] == 0) {
                 flowerbed[i] = 1;
